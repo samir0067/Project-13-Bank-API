33e00d7cf021c33585b07c0232d9fdab680ffe00
samir0067 <arrahmani.samir@gmail.com>
2022-10-31 11:43:46 +0100

    fix connection

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+const mongoose = require("mongoose");
+const dotEnv = require("dotenv");
 
-module.exports = async () => {
+dotEnv.config();
+
+const databaseUrl =
+  process.env.DATABASE_URL || "mongodb://localhost/argentBankDB";
+
+module.exports = async() => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, {useUnifiedTopology: true, useNewUrlParser: true});
+    console.log("== Database successfully connected ==");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`<== Database Connectivity Error: ${error} ==>`);
+    throw new Error(error);
   }
-}
+};
--- server/database/models/userModel.js
+++ server/database/models/userModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
@@ -10,15 +10,15 @@ const userSchema = new mongoose.Schema(
   {
     timestamps: true,
     toObject: {
-      transform: (doc, ret, options) => {
-        ret.id = ret._id
-        delete ret._id
-        delete ret.password
-        delete ret.__v
-        return ret
+      transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+        return ret;
       }
     }
   }
-)
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
